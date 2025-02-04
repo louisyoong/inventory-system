4677f4fe4104f0db28edb91e983430da87639696
louisyoong <louis910729@gmail.com>
2025-02-04 17:29:55 +0800

    Fix bug

--- src/app/component/Header.tsx
+++ src/app/component/Header.tsx
@@ -1,10 +1,8 @@
 import { AppBar, Toolbar, Typography, Button } from "@mui/material";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useAuth } from "@/context/AuthContext";
 
 function Header() {
-  const { user } = useAuth();
   const router = useRouter();
 
   const handleLogout = () => {
